6cbd3fe19d9c7218252213dd06d0ba57797ca589
sharl <l_sharan@yahoo.com>
2015-09-27 22:38:17 -0700

    Added evaluation wrt AlchemyAPI using [Jean-Louis14] methodology

--- evaluatealchemy.py
+++ evaluatealchemy.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 """
-Evaluate keyword extraction model (and the underlying keyword classification model) 
-on held out set of Crowd500 dataset [Marujo2012]
+Evaluate AlchemyAPI keyword extraction method on held out set of Crowd500 dataset [Marujo2012]
 """
 
-from keywordextraction import *
+import json
 import pickle
+import numpy as np
+from keywordextraction import get_crowdd500_data,evaluate_keywords
 
-def main():  
-  preload = 1
+
+def main():
   top_k = 15
   verbose = 1
 
-  # get keyword classifier
-  keyword_classifier = get_keywordclassifier(preload)['model']
+  # load saved alchemy api responses
+  alchemy_keywords = pickle.load(open('saved/alchemy_crowd500_keywords.pkl','rb'))
 
   # evaluate on Crowd500 test set
   testdata = get_crowdd500_data('test')
@@ -21,32 +22,29 @@ def main():
   
   performance_data = np.zeros((num_docs,3))
   for doc_idx in xrange(num_docs):
-    text = testdata['documents'][doc_idx]
     true_keywords = testdata['keywords'][doc_idx]
-    if doc_idx == 0:
-      suggested_keywords = extract_keywords(text,keyword_classifier,top_k,preload)
-    else:
-      # if preload = 0, save time here as tf-idf transformation of training data 
-      # only has to be computed once
-      suggested_keywords = extract_keywords(text,keyword_classifier,top_k,1)
+    suggested_keywords = []
+    for (keyword,score) in alchemy_keywords[doc_idx]:
+      suggested_keywords.extend(keyword.lower().split())
 
-    (precision,recall,f1score) = evaluate_keywords(suggested_keywords,true_keywords)
+    (precision,recall,f1score) = evaluate_keywords(suggested_keywords[:top_k],true_keywords)
     performance_data[doc_idx,0] = precision
     performance_data[doc_idx,1] = recall
     performance_data[doc_idx,2] = f1score
     if verbose == 1:
       print 'Document %d of %d: f1-score for top-%d keywords extracted by model = %.4f' % (doc_idx,num_docs,top_k,f1score)
 
-  print '--------------------------------------------------------------'
-  print 'Evaluation of keyword extraction model on Crowd500 test set'
+  print '----------------------------------------------------------------'
+  print 'Evaluation of AlchemyAPI keyword extraction on Crowd500 test set'
   print 'Number of documents = %d, keywords extracted per document = %d' % (num_docs,top_k)
   print 'Precision: Mean = %.4f, SEM = %.4f' % (np.mean(performance_data[:,0]),np.std(performance_data[:,0])/float(np.sqrt(num_docs)))
   print 'Recall: Mean = %.4f, SEM = %.4f' % (np.mean(performance_data[:,1]),np.std(performance_data[:,1])/float(np.sqrt(num_docs)))
   print 'F-1 score: Mean = %.4f, SEM = %.4f' % (np.mean(performance_data[:,2]),np.std(performance_data[:,2])/float(np.sqrt(num_docs)))
-  print '--------------------------------------------------------------'
-  
-  pickle.dump(performance_data,open('saved/model_evaluation.pkl','wb'))
+  print '----------------------------------------------------------------'
+
+  pickle.dump(performance_data,open('saved/alchemy_evaluation.pkl','wb'))
   return
+
 
 if __name__ == '__main__':
   main()
